b73621a010e136e62dbc813299a78403b95e29df
Pratheek Unni <pratheekastro3@gmail.com>
2026-04-08 23:16:55 +0530

    ci: add github actions workflow for github pages deployment
    - Created \.github/workflows/nextjs.yml\ to automate build and deploy to GitHub Pages.
    
    - Updated \
    ext.config.ts\ to enable static export (\output: 'export'\) and dynamically configure \asePath\ for GitHub Pages.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = "";
+
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  repo = `/${process.env.GITHUB_REPOSITORY.split("/")[1]}`;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: repo,
+  assetPrefix: repo,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
